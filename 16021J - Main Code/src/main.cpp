dffb6d5018a7497e102dab6508b2e6228da1632b
Mr-Kurtis72 <caydenwyatt07@hotmail.com>
2024-12-06 13:50:58 -0600

    added test autonomous

--- 16021J - Main Code/src/main.cpp
+++ 16021J - Main Code/src/main.cpp
@@ -2,6 +2,7 @@
 #include "lemlib/api.hpp"
 #include "lemlib/chassis/trackingWheel.hpp"
 #include "pros/abstract_motor.hpp"
+#include "pros/misc.h"
 #include <cstdio>
 #include <unordered_map>
 
@@ -10,7 +11,8 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 static std::unordered_map<std::string, pros::controller_digital_e_t> buttonMap = {
     {"Stake Lock", pros::E_CONTROLLER_DIGITAL_L1},
-    {"Conveyer Spin", pros::E_CONTROLLER_DIGITAL_R1}
+    {"Conveyer Spin", pros::E_CONTROLLER_DIGITAL_R1},
+    {"Reverse Conveyer Spin", pros::E_CONTROLLER_DIGITAL_R2}
 };
 
 int intake_velocity = 127;
@@ -20,32 +22,34 @@ int intake_velocity = 127;
 pros::adi::DigitalOut stake_lock('E');
 
 //intake motor
-pros::Motor intake_motor (1);
+pros::Motor intake_motor1 (1);
+pros::Motor intake_motor2 (5);
+
 
 // left motor group
-pros::MotorGroup left_motor_group({-3, -7, -6}, pros::MotorGears::blue);
+pros::MotorGroup left_motor_group({-7, -6}, pros::MotorGears::blue);
 // right motor group
-pros::MotorGroup right_motor_group({18, 19, 20}, pros::MotorGears::blue);
+pros::MotorGroup right_motor_group({18, 19}, pros::MotorGears::blue);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
                               &right_motor_group, // right motor group
-                              10, // 10 inch track width
+                              12.75, // 10 inch track width
                               lemlib::Omniwheel::NEW_325, // using new 4" omnis
                               360, // drivetrain rpm is 360
                               2 // horizontal drift is 2 (for now)
 );
 
 // imu
-pros::Imu imu(10);
+pros::Imu imu(11);
 // horizontal tracking wheel encoder
-pros::adi::Encoder horizontal_encoder('A', 'B', true);
+pros::adi::Encoder horizontal_encoder('A', 'B', false);
 // vertical tracking wheel encoder
-pros::adi::Encoder vertical_encoder('C', 'D', true);
+pros::adi::Encoder vertical_encoder('G', 'H', false);
 // horizontal tracking wheel
-lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_275, -5.75);
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_encoder, lemlib::Omniwheel::NEW_275, 0);
 // vertical tracking wheel
-lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, -2.5);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, 0);
 
 // odometry settings
 lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
@@ -105,8 +109,15 @@ void conveyer_spin(){
     //static bool pressed = false;
     while(true){
         if(controller.get_digital(buttonMap["Conveyer Spin"])){
-            intake_motor.move(intake_velocity);
-        } else intake_motor.brake();
+            intake_motor1.move(intake_velocity);
+            intake_motor2.move(intake_velocity);
+        } else if(controller.get_digital(buttonMap["Reverse Conveyer Spin"])){
+            intake_motor1.move(-intake_velocity);
+            intake_motor2.move(-intake_velocity);
+        } else{
+            intake_motor1.brake();
+            intake_motor2.brake();
+        }
         pros::c::delay(25);
     }
 }
@@ -115,6 +126,8 @@ void conveyer_spin(){
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
+    chassis.setPose(-60, 0, 90);// set chassis pose
+
     // print position to brain screen
     pros::Task screen_task([&]() {
         while (true) {
@@ -140,9 +153,9 @@ void on_center_button() {
 	static bool pressed = false;
 	pressed = !pressed;
 	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
+		pros::lcd::set_text(3, "I was pressed!");
 	} else {
-		pros::lcd::clear_line(2);
+		pros::lcd::clear_line(3);
 	}
 }
 
@@ -182,8 +195,18 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
 
+// path file name is "example.txt".
+// "." is replaced with "_" to overcome c++ limitations
+ASSET(path_txt);
+
+void autonomous() {
+    
+    // lookahead distance: 15 inches
+    // timeout: 2000 ms
+    chassis.follow(path_txt, 15, 10000);
+    // follow the next path, but with the robot going backwards
+}
 /**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
